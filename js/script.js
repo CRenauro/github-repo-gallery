c01eea3acc1ba2267f23ed370579d40766a6d429
CRenauro <crenauro@optimum.net>
2021-06-07 14:40:14 -0400

    fetched user data

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 const overview = document.querySelector(".overview");
 const username = "CRenauro";
+const repoList = document.querySelector(".repo-list");
 
 const githubUserInfo = async function () {
-    const userInfo = await fetch(`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
-    displayUserInfo(data);
-    // console.log(userInfo);
-}; 
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+  displayUserInfo(data);
+  // console.log(userInfo);
+};
 
 githubUserInfo();
 
 const displayUserInfo = function (data) {
-    const div = document.createElement("div");
-    div.classList.add("user-info");
-    div.innerHTML = `
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
       <figure>
         <img alt="user avatar" src=${data.avatar_url} />
       </figure>
@@ -24,5 +25,23 @@ const displayUserInfo = function (data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
       </div>
     `;
-    overview.append(div);
-  };
+  overview.append(div);
+  gitRepos();
+};
+
+const gitRepos = async function () {
+  const fetchRepos = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+  );
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
